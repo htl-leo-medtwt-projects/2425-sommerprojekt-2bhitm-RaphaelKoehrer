aafdf119847963056f9cf335ca80f5dbd1ed8293
Raphael Koehrer <r.koehrer@students.htl-leonding.ac.at>
2025-04-03 16:04:50 +0200

    Inputs erstellt, Buttons Design

--- project/site/configSite/script.js
+++ project/site/configSite/script.js
@@ -7,3 +7,25 @@ function visitNextSite() {
 function showTut() {
      document.getElementById('tutorial').style.display = 'flex';
 }
+function chooseTeam(team) {
+     const humanButton = document.querySelector("#chooseTeam div:nth-child(2) p");
+     const orcButton = document.querySelector("#chooseTeam div:nth-child(3) p");
+ 
+     if (team === 'human') {
+          humanButton.classList.remove("yellow");
+          orcButton.classList.remove("yellow");
+
+          humanButton.classList.remove("gray");
+         humanButton.classList.add("white");
+         orcButton.classList.remove("white");
+         orcButton.classList.add("gray");
+     } else if (team === 'orc') {
+          humanButton.classList.remove("yellow");
+          orcButton.classList.remove("yellow");
+
+          humanButton.classList.remove("white");
+          humanButton.classList.add("gray");
+          orcButton.classList.remove("gray");
+          orcButton.classList.add("white");
+     }
+ }
